bb6d5c342e984ac690314d4bd30c45ff0aa4287b
Potmot <377029227@qq.com>
2024-01-24 15:51:59 +0800

    2024-1-24 移除 error page 覆盖，前端继续采用 hash 模式

--- src/main/kotlin/top/potmot/config/WebConfig.kt
+++ src/main/kotlin/top/potmot/config/WebConfig.kt
@@ -1,10 +1,6 @@
 package top.potmot.config
 
-import org.springframework.boot.web.server.ErrorPage
-import org.springframework.boot.web.server.ErrorPageRegistrar
-import org.springframework.boot.web.server.ErrorPageRegistry
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -13,15 +9,10 @@ import kotlin.reflect.full.findAnnotation
 const val API = "/api"
 
 @Configuration
-class WebConfig : WebMvcConfigurer, ErrorPageRegistrar {
+class WebConfig : WebMvcConfigurer {
     override fun configurePathMatch(configurer: PathMatchConfigurer) {
         configurer.addPathPrefix(API) {controller ->
             controller.kotlin.findAnnotation<RestController>() != null
         }
-    }
-
-    override fun registerErrorPages(registry: ErrorPageRegistry) {
-        val notFoundPage = ErrorPage(HttpStatus.NOT_FOUND, "/index.html")
-        registry.addErrorPages(notFoundPage)
     }
 }
