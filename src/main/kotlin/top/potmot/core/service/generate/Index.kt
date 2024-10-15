c7705aa8da958213d6455468b9e40fa0556965d6
Potmot <377029227@qq.com>
2024-10-15 10:55:35 +0800

    2024-10-15 添加 ServiceGenerator Index

--- src/main/kotlin/top/potmot/core/service/generate/Index.kt
+++ src/main/kotlin/top/potmot/core/service/generate/Index.kt
@@ -1,2 +1,11 @@
 package top.potmot.core.service.generate
 
+import top.potmot.core.service.generate.impl.java.JavaServiceCodeGenerator
+import top.potmot.core.service.generate.impl.kotlin.KotlinServiceCodeGenerator
+import top.potmot.enumeration.GenLanguage
+
+fun GenLanguage.getServiceGenerator(): ServiceCodeGenerator =
+    when (this) {
+        GenLanguage.KOTLIN -> KotlinServiceCodeGenerator
+        GenLanguage.JAVA -> JavaServiceCodeGenerator
+    }
