9e762c233be56d61f088a918d14595637edbed49
Potmot <377029227@qq.com>
2024-11-08 18:10:58 +0800

    UPDATE: view Constant

--- src/main/kotlin/top/potmot/core/business/view/generate/Constant.kt
+++ src/main/kotlin/top/potmot/core/business/view/generate/Constant.kt
@@ -2,4 +2,14 @@ package top.potmot.core.business.view.generate
 
 const val generatePath = "@/api/__generated"
 
+const val staticPath = "@/api/__generated/model/static"
+
+const val enumPath = "@/api/__generated/model/enums"
+
+const val servicePath = "@/api/__generated/services"
+
+const val rulePath = "@/rules"
+
 const val componentPath = "@/components"
+
+const val pagePath = "@/pages"
