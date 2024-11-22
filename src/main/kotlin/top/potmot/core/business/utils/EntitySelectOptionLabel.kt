25ce8eaf55c4de5e836e86ca1727e50cafe8b600
Potmot <377029227@qq.com>
2024-11-22 15:16:36 +0800

    UPDATE: 使用 inOptionView 替代硬编码 selectOptionLabel

--- src/main/kotlin/top/potmot/core/business/utils/EntitySelectOptionLabel.kt
+++ src/main/kotlin/top/potmot/core/business/utils/EntitySelectOptionLabel.kt
@@ -2,23 +2,7 @@ package top.potmot.core.business.utils
 
 import top.potmot.entity.dto.GenEntityBusinessView
 
-// 备选 select option label
-private val alternativeSelectOptionLabel = setOf(
-    "name",
-    "label",
-    "title",
-    "comment",
-)
-
 val GenEntityBusinessView.selectOptionLabel: String?
-    get() {
-        val propertyNames = properties
-            .filter { !it.idProperty && it.associationType == null && it.entityId == id }
-            .map { it.name }.toSet()
-
-        for (item in alternativeSelectOptionLabel) {
-            if (item in propertyNames) return item
-        }
-
-        return null
-    }
+    get() = properties
+        .firstOrNull { it.inOptionView }
+        ?.name
