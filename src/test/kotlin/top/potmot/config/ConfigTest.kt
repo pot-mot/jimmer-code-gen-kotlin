d0cfc8cccc53f6d2324198a15b15a9c5928e52bf
Potmot <377029227@qq.com>
2024-12-28 20:09:05 +0800

    FIX: 修正 globalGenConfig 的测试

--- src/test/kotlin/top/potmot/config/ConfigTest.kt
+++ src/test/kotlin/top/potmot/config/ConfigTest.kt
@@ -13,7 +13,8 @@ import top.potmot.service.ConfigService
 @SpringBootTest
 @ActiveProfiles("test", "h2")
 class AssociationMatchTest(
-    @Autowired val configService: ConfigService
+    @Autowired val configService: ConfigService,
+    @Autowired val globalGenConfig: GlobalGenConfig
 ) {
     /**
      * 测试配置编辑功能是否符合预期
@@ -21,25 +22,25 @@ class AssociationMatchTest(
     @Test
     fun testConfigEdit() {
         useContext {
-            assertEquals(GenLanguage.KOTLIN, GlobalGenConfig.language)
+            assertEquals(GenLanguage.KOTLIN, globalGenConfig.language)
 
             val testAuthor = "AUTHOR"
-            val oldAuthor = GlobalGenConfig.author
+            val oldAuthor = globalGenConfig.author
             configService.setConfig(GenConfigProperties(author = testAuthor))
-            assertEquals(testAuthor, GlobalGenConfig.author)
+            assertEquals(testAuthor, globalGenConfig.author)
             configService.setConfig(GenConfigProperties(author = oldAuthor))
 
-            val oldLanguage = GlobalGenConfig.language
+            val oldLanguage = globalGenConfig.language
             configService.setConfig(GenConfigProperties(language = GenLanguage.JAVA))
-            assertEquals(GenLanguage.JAVA, GlobalGenConfig.language)
+            assertEquals(GenLanguage.JAVA, globalGenConfig.language)
             configService.setConfig(GenConfigProperties(language = oldLanguage))
 
             val testColumnPrefix = "C_"
-            val oldColumnPrefix = GlobalGenConfig.columnNamePrefixes
+            val oldColumnPrefix = globalGenConfig.columnNamePrefixes
             configService.setConfig(GenConfigProperties(columnNamePrefixes = testColumnPrefix))
-            assertEquals(testColumnPrefix, GlobalGenConfig.columnNamePrefixes)
+            assertEquals(testColumnPrefix, globalGenConfig.columnNamePrefixes)
             configService.setConfig(GenConfigProperties(columnNamePrefixes = oldColumnPrefix))
-            assertEquals(oldColumnPrefix, GlobalGenConfig.columnNamePrefixes)
+            assertEquals(oldColumnPrefix, globalGenConfig.columnNamePrefixes)
 
         }
     }
