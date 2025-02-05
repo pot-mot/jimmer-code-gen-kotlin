35625fce1ebc35c5db1c2602a663e6ba87baf306
Potmot <377029227@qq.com>
2025-02-05 17:35:50 +0800

    UPDATE: 整理业务配置相关代码，移除不必要的 core/model/business，将全部实体业务配置相关代码聚合至 EntityBusinessConfig

--- src/main/kotlin/top/potmot/core/entity/business/EntityBusinessConfig.kt
+++ src/main/kotlin/top/potmot/core/entity/business/EntityBusinessConfig.kt
@@ -2,9 +2,209 @@ package top.potmot.core.entity.business
 
 import org.babyfish.jimmer.sql.ast.mutation.AssociatedSaveMode
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.expression.eq
 import org.springframework.transaction.support.TransactionTemplate
-import top.potmot.core.model.business.EntityModelBusinessInput
+import top.potmot.entity.GenEnum
+import top.potmot.entity.GenTable
+import top.potmot.entity.dto.GenEntityConfigInput
+import top.potmot.entity.dto.GenEntityModelView
+import top.potmot.entity.dto.GenEnumModelBusinessFillView
+import top.potmot.entity.dto.GenPropertyEntityConfigInput
+import top.potmot.entity.dto.GenPropertyModelView
+import top.potmot.entity.dto.GenTableModelBusinessFillView
+import top.potmot.entity.dto.IdName
 import top.potmot.entity.dto.toEntities
+import top.potmot.entity.modelId
+import top.potmot.error.ModelBusinessInputException
+
+/**
+ * 实体 - 用于模型业务配置的输入
+ * 将直接被用于 save
+ */
+data class EntityModelBusinessInput(
+    /**
+     * 实体业务配置输入，仅包含特定的业务配置项
+     */
+    val entity: GenEntityConfigInput,
+
+    /**
+     * 属性业务配置输入，具有全标量字段，用于编辑无需考虑关联的非转换属性
+     */
+    val properties: List<GenPropertyEntityConfigInput>,
+)
+
+/**
+ * 实体 - 用于模型业务配置的输出
+ * 同时也将在 JSON 保存时作为输入对象，此时将被被转换为 EntityModelBusinessInput
+ */
+data class EntityModelBusinessView(
+    /**
+     * 表直接转换得到的实体
+     */
+    val tableConvertedEntity: GenEntityModelView,
+
+    /**
+     * 其余非转换属性
+     */
+    val otherProperties: List<GenPropertyModelView>,
+)
+
+@Throws(
+    ModelBusinessInputException.PropertyCannotMatchColumn::class,
+    ModelBusinessInputException.PropertyCannotRematchOldProperty::class,
+    ModelBusinessInputException.PropertyMatchedMoreThanOneOldProperty::class,
+)
+private fun GenEntityModelView.toConfigInput(
+    id: Long,
+    columnNameMap: Map<String, GenTableModelBusinessFillView.TargetOf_columns>,
+) = GenEntityConfigInput(
+    id = id,
+    name = name,
+    overwriteName = overwriteName,
+    comment = comment,
+    overwriteComment = overwriteComment,
+    remark = remark,
+    canAdd = canAdd,
+    canEdit = canEdit,
+    canQuery = canQuery,
+    canDelete = canDelete,
+    hasPage = hasPage,
+    otherAnnotation = otherAnnotation,
+    properties = properties.map { property ->
+        val column = columnNameMap[property.columnName]
+            ?: throw ModelBusinessInputException.propertyCannotMatchColumn(
+                "Property [${property.name}] cannot match column",
+                entity = IdName(id, name),
+                propertyName = property.name,
+            )
+
+        val matchProperties = column.properties.filter {
+            (if (it.overwriteName || property.overwriteName) true else it.name == property.name)
+                    && it.type == property.type
+                    && it.typeNotNull == property.typeNotNull
+                    && it.typeTable?.name == property.typeTable?.name
+                    && it.associationType == property.associationType
+                    && it.joinColumnMetas.toString() == property.joinColumnMetas.toString()
+                    && it.joinTableMeta.toString() == property.joinTableMeta.toString()
+        }
+
+        if (matchProperties.isEmpty())
+            throw ModelBusinessInputException.propertyCannotRematchOldProperty(
+                "Property [${property.name}] matched column [${column.name}] has no properties",
+                entity = IdName(id, name),
+                propertyName = property.name,
+                matchedColumn = IdName(column.id, column.name)
+            )
+
+        if (matchProperties.size > 1)
+            throw ModelBusinessInputException.propertyMatchedMoreThanOneOldProperty(
+                "Property [${property.name}] matched column [${column.name}] has more than one matched properties:" +
+                        matchProperties.joinToString(", ") { "[${property.name}]" },
+                entity = IdName(id, name),
+                propertyName = property.name,
+                matchedColumn = IdName(column.id, column.name),
+                matchedProperties = matchProperties.map { IdName(it.id, it.name) }
+            )
+
+        val matchProperty = matchProperties[0]
+
+        GenEntityConfigInput.TargetOf_properties(
+            id = matchProperty.id,
+            name = property.name,
+            overwriteName = property.overwriteName,
+            comment = property.comment,
+            overwriteComment = property.overwriteComment,
+            remark = property.remark,
+            otherAnnotation = property.otherAnnotation,
+            orderKey = property.orderKey,
+            longAssociation = property.longAssociation,
+            inListView = property.inListView,
+            inDetailView = property.inDetailView,
+            inInsertInput = property.inInsertInput,
+            inUpdateInput = property.inUpdateInput,
+            inOptionView = property.inOptionView,
+            inSpecification = property.inSpecification,
+            inShortAssociationView = property.inShortAssociationView,
+            inLongAssociationInput = property.inLongAssociationInput,
+            inLongAssociationView = property.inLongAssociationView,
+        )
+    }
+)
+
+private fun GenPropertyModelView.toConfigInput(
+    enumNameIdMap: Map<String, Long>,
+) = GenPropertyEntityConfigInput(
+    name = name,
+    overwriteName = overwriteName,
+    comment = comment,
+    overwriteComment = overwriteComment,
+    type = type,
+    listType = listType,
+    typeNotNull = typeNotNull,
+    idProperty = idProperty,
+    idGenerationAnnotation = idGenerationAnnotation,
+    keyProperty = keyProperty,
+    keyGroup = keyGroup,
+    logicalDelete = logicalDelete,
+    idView = idView,
+    idViewTarget = idViewTarget,
+    associationType = associationType,
+    longAssociation = longAssociation,
+    mappedBy = mappedBy,
+    inputNotNull = inputNotNull,
+    joinColumnMetas = joinColumnMetas,
+    joinTableMeta = joinTableMeta,
+    dissociateAnnotation = dissociateAnnotation,
+    otherAnnotation = otherAnnotation,
+    body = body,
+    orderKey = orderKey,
+    inListView = inListView,
+    inDetailView = inDetailView,
+    inInsertInput = inInsertInput,
+    inUpdateInput = inUpdateInput,
+    inSpecification = inSpecification,
+    inOptionView = inOptionView,
+    inShortAssociationView = inShortAssociationView,
+    inLongAssociationView = inLongAssociationView,
+    inLongAssociationInput = inLongAssociationInput,
+    remark = remark,
+    enumId = enumNameIdMap[enum?.name]
+)
+
+@Throws(ModelBusinessInputException::class)
+private fun List<EntityModelBusinessView>.toInputs(
+    tables: List<GenTableModelBusinessFillView>,
+    enums: List<GenEnumModelBusinessFillView>,
+): List<EntityModelBusinessInput> {
+    val tableNameMap = tables.associateBy { it.name }
+    val enumNameIdMap = enums.associate { it.name to it.id }
+
+    return map { (entity, properties) ->
+        val table = tableNameMap[entity.tableName]
+            ?: throw ModelBusinessInputException.entityCannotMatchTable(
+                "entity [${entity.name}] cannot match table",
+                entityName = entity.name,
+                tableName = entity.tableName
+            )
+        val entityId = table.entityId
+            ?: throw ModelBusinessInputException.entityMatchedTableConvertedEntityNotFound(
+                entityName = entity.name,
+                table = IdName(table.id, table.name)
+            )
+
+        val columnNameMap = table.columns.associateBy { it.name }
+
+        EntityModelBusinessInput(
+            entity.toConfigInput(
+                entityId,
+                columnNameMap
+            ),
+            properties.map { property ->
+                property.toConfigInput(enumNameIdMap)
+            }
+        )
+    }
+}
 
 interface EntityBusinessConfig {
     val transactionTemplate: TransactionTemplate
@@ -37,5 +237,25 @@ interface EntityBusinessConfig {
             columnId = null
             typeTable = null
         })
+    }
+
+    @Throws(ModelBusinessInputException::class)
+    fun KSqlClient.configEntities(
+        modelId: Long,
+        views: List<EntityModelBusinessView>
+    ) {
+        val tables = executeQuery(GenTable::class) {
+            where(table.modelId eq modelId)
+            select(table.fetch(GenTableModelBusinessFillView::class))
+        }
+
+        val enums = executeQuery(GenEnum::class) {
+            where(table.modelId eq modelId)
+            select(table.fetch(GenEnumModelBusinessFillView::class))
+        }
+
+        val inputs = views.toInputs(tables, enums)
+
+        configEntities(inputs)
     }
 }
--- src/main/kotlin/top/potmot/service/ModelService.kt
+++ src/main/kotlin/top/potmot/service/ModelService.kt
@@ -1,6 +1,12 @@
 package top.potmot.service
 
 import org.babyfish.jimmer.sql.kt.KSqlClient
+import org.babyfish.jimmer.sql.kt.ast.expression.eq
+import org.babyfish.jimmer.sql.kt.ast.expression.valueIn
+import org.babyfish.jimmer.sql.kt.ast.expression.valueNotIn
+import org.babyfish.jimmer.sql.kt.ast.table.isNotNull
+import org.babyfish.jimmer.sql.kt.ast.table.isNull
+import org.babyfish.jimmer.sql.kt.fetcher.newFetcher
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.transaction.support.TransactionTemplate
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -12,17 +18,25 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import top.potmot.core.entity.business.EntityBusinessConfig
-import top.potmot.core.model.business.EntityModelBusinessView
-import top.potmot.core.model.business.ModelBusinessInput
-import top.potmot.core.model.business.ModelBusinessOutput
+import top.potmot.core.entity.business.EntityModelBusinessView
 import top.potmot.core.model.load.ModelInputEntities
 import top.potmot.core.model.load.ModelSave
 import top.potmot.core.model.load.getGraphEntities
+import top.potmot.entity.GenEntity
 import top.potmot.entity.GenModel
+import top.potmot.entity.GenProperty
+import top.potmot.entity.by
+import top.potmot.entity.column
+import top.potmot.entity.`column?`
 import top.potmot.entity.createdTime
+import top.potmot.entity.dto.GenEntityModelView
 import top.potmot.entity.dto.GenModelInput
 import top.potmot.entity.dto.GenModelSimpleView
 import top.potmot.entity.dto.GenModelView
+import top.potmot.entity.dto.GenPropertyModelView
+import top.potmot.entity.entityId
+import top.potmot.entity.id
+import top.potmot.entity.modelId
 import top.potmot.error.LoadFromModelException
 import top.potmot.error.ModelBusinessInputException
 import top.potmot.utils.transaction.executeNotNull
@@ -35,9 +49,14 @@ class ModelService(
     @Autowired
     override val transactionTemplate: TransactionTemplate,
 ) : ModelSave,
-    ModelBusinessInput,
-    ModelBusinessOutput,
     EntityBusinessConfig {
+    @GetMapping
+    fun list(): List<GenModelSimpleView> =
+        sqlClient.executeQuery(GenModel::class) {
+            orderBy(table.createdTime)
+            select(table.fetch(GenModelSimpleView::class))
+        }
+
     @GetMapping("/{id}")
     fun get(@PathVariable id: Long): GenModelView? =
         sqlClient.findById(GenModelView::class, id)
@@ -46,19 +65,45 @@ class ModelService(
     fun getValueData(@PathVariable id: Long): ModelInputEntities? =
         sqlClient.findById(GenModelView::class, id)?.getGraphEntities()
 
+    private val entityModelViewFetcher = newFetcher(GenEntity::class).by(GenEntityModelView.METADATA.fetcher) {
+        properties(
+            GenEntityModelView.TargetOf_properties.METADATA.fetcher
+        ) {
+            filter {
+                where(table.column.isNotNull())
+            }
+        }
+    }
+
     @GetMapping("/entityBusinessViews/{id}")
     fun getEntityBusinessViews(
         @PathVariable id: Long,
         @RequestParam(required = false) excludeEntityIds: List<Long>?,
-    ): List<EntityModelBusinessView> =
-        sqlClient.getEntityModelBusinessViews(id, excludeEntityIds)
-
-    @GetMapping
-    fun list(): List<GenModelSimpleView> =
-        sqlClient.executeQuery(GenModel::class) {
-            orderBy(table.createdTime)
-            select(table.fetch(GenModelSimpleView::class))
+    ): List<EntityModelBusinessView> {
+        val entities = sqlClient.executeQuery(GenEntity::class) {
+            where(table.modelId eq id)
+            excludeEntityIds?.takeIf { it.isNotEmpty() }?.let {
+                where(table.id valueNotIn it)
+            }
+            select(table.fetch(entityModelViewFetcher))
         }
+
+        val propertiesMap = sqlClient.executeQuery(GenProperty::class) {
+            where(table.`column?`.isNull())
+            where(table.entityId valueIn entities.map { it.id })
+            select(table.entityId, table.fetch(GenPropertyModelView.METADATA.fetcher))
+        }
+            .groupBy { it._1 }
+
+        return entities.map {
+            EntityModelBusinessView(
+                GenEntityModelView(it),
+                propertiesMap[it.id]
+                    ?.map { (_, property) -> GenPropertyModelView(property) }
+                    ?: emptyList()
+            )
+        }
+    }
 
     @PostMapping
     @Throws(LoadFromModelException::class)
@@ -72,14 +117,8 @@ class ModelService(
     fun saveBusiness(
         @PathVariable id: Long,
         @RequestBody entities: List<EntityModelBusinessView>,
-    ) {
-        transactionTemplate.execute {
-            val entityModelBusinessInputs =
-                sqlClient.createEntityModelBusinessInputs(id, entities)
-
-            sqlClient.configEntities(entityModelBusinessInputs)
-        }
-    }
+    ) =
+        sqlClient.configEntities(id, entities)
 
     @DeleteMapping
     fun delete(@RequestParam ids: List<Long>): Int =
