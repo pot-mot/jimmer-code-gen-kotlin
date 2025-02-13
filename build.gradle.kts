355813f6b093374a1f571d3f5ed5cf0f86b2a77c
Potmot <377029227@qq.com>
2025-02-13 14:17:05 +0800

    UPGRADE: 升级 jimmer 至 0.9.51

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ group = "top.potmot"
 version = "0.2.1" // 2025-2-10
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
-val jimmerVersion = "0.9.49"
+val jimmerVersion = "0.9.51"
 
 val mysqlVersion = "9.0.0"
 val postgresVersion = "42.7.3"
