2aa752dc8088d5ea678919b916477f6bba4527f5
Potmot <377029227@qq.com>
2025-03-09 17:46:18 +0800

    UPDATE: 为 GenModelSubGroup 添加 Key

--- src/main/kotlin/top/potmot/entity/GenModelSubGroup.kt
+++ src/main/kotlin/top/potmot/entity/GenModelSubGroup.kt
@@ -5,7 +5,6 @@ import org.babyfish.jimmer.sql.GeneratedValue
 import org.babyfish.jimmer.sql.GenerationType
 import org.babyfish.jimmer.sql.Id
 import org.babyfish.jimmer.sql.IdView
-import org.babyfish.jimmer.sql.JoinColumn
 import org.babyfish.jimmer.sql.Key
 import org.babyfish.jimmer.sql.ManyToOne
 import org.babyfish.jimmer.sql.OneToMany
