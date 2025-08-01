eae8004e2e9552fc521b764448ebae2fea7dcb1c
Potmot <377029227@qq.com>
2025-08-02 01:31:44 +0800

    FIX: PermissionGenerator sql

--- src/main/kotlin/top/potmot/core/business/permission/generate/PermissionGenerator.kt
+++ src/main/kotlin/top/potmot/core/business/permission/generate/PermissionGenerator.kt
@@ -32,17 +32,17 @@ object PermissionGenerator {
                 content = buildString {
                     appendBlock(
                         """
-DELETE FROM sys_user_sys_role_mapping WHERE sys_role_id IN (
+DELETE FROM sys_user_sys_role_mapping WHERE role_id IN (
     SELECT id FROM sys_role
     WHERE name = '${role}'
 );
 
-DELETE FROM sys_role_sys_permission_mapping WHERE sys_role_id IN (
+DELETE FROM sys_role_sys_permission_mapping WHERE role_id IN (
     SELECT id FROM sys_role
     WHERE name = '${role}'
 );
 
-DELETE FROM sys_menu_sys_permission_mapping WHERE sys_permission_id IN (
+DELETE FROM sys_menu_sys_permission_mapping WHERE permission_id IN (
     SELECT id FROM sys_permission
     WHERE name IN ('${menuPermission}')
 );
@@ -53,22 +53,22 @@ WHERE name IN ('${menuPermission}');
 DELETE FROM sys_role
 WHERE name = '${role}';
 
-INSERT INTO sys_role (name, created_by, created_time, modified_by, modified_time)
-VALUES ('${role}', 1, now(), 1, now());
+INSERT INTO sys_role (name, created_time, modified_time)
+VALUES ('${role}', now(), now());
 
-INSERT INTO sys_user_sys_role_mapping (sys_role_id, sys_user_id)
+INSERT INTO sys_user_sys_role_mapping (role_id, user_id)
 VALUES ((SELECT id FROM sys_role WHERE name = '${role}' LIMIT 1), 1);
 """
                     )
 
                     appendLines(
-                        "INSERT INTO sys_permission (name, created_by, created_time, modified_by, modified_time)",
-                        "VALUES ('${menuPermission}', 1, now(), 1, now());",
+                        "INSERT INTO sys_permission (name, created_time, modified_time)",
+                        "VALUES ('${menuPermission}', now(), now());",
                     )
 
                     appendBlock(
                         """
-INSERT INTO sys_role_sys_permission_mapping (sys_role_id, sys_permission_id)
+INSERT INTO sys_role_sys_permission_mapping (role_id, permission_id)
 SELECT (SELECT id FROM sys_role WHERE name = '${role}' LIMIT 1), id FROM sys_permission 
 WHERE sys_permission.name IN ('${menuPermission}');
 """
@@ -92,12 +92,12 @@ WHERE sys_permission.name IN ('${menuPermission}');
                 buildString {
                     appendBlock(
                         """
-DELETE FROM sys_user_sys_role_mapping WHERE sys_role_id IN (
+DELETE FROM sys_user_sys_role_mapping WHERE role_id IN (
     SELECT id FROM sys_role
     WHERE name = '${role}'
 );
 
-DELETE FROM sys_role_sys_permission_mapping WHERE sys_role_id IN (
+DELETE FROM sys_role_sys_permission_mapping WHERE role_id IN (
     SELECT id FROM sys_role
     WHERE name = '${role}'
 );
@@ -110,7 +110,7 @@ WHERE name = '${role}';
                     if (allPermissions.isNotEmpty()) {
                         appendBlock(
                             """
-DELETE FROM sys_menu_sys_permission_mapping WHERE sys_permission_id IN (
+DELETE FROM sys_menu_sys_permission_mapping WHERE permission_id IN (
     SELECT id FROM sys_permission
     WHERE name IN (${allPermissions.joinToString(", ") { permission -> "'$permission'" }})
 );
@@ -123,24 +123,24 @@ WHERE name IN (${allPermissions.joinToString(", ") { permission -> "'$permission
 
                     if (permissions.isNotEmpty()) {
                         appendLines(
-                            "INSERT INTO sys_role (name, created_by, created_time, modified_by, modified_time)",
-                            "VALUES ('${role}', 1, now(), 1, now());",
+                            "INSERT INTO sys_role (name, created_time, modified_time)",
+                            "VALUES ('${role}', now(), now());",
                             "",
-                            "INSERT INTO sys_user_sys_role_mapping (sys_role_id, sys_user_id)",
+                            "INSERT INTO sys_user_sys_role_mapping (role_id, user_id)",
                             "VALUES ((SELECT id FROM sys_role WHERE name = '${role}' LIMIT 1), 1);",
                             ""
                         )
 
                         for (permission in permissions) {
                             appendLines(
-                                "INSERT INTO sys_permission (name, created_by, created_time, modified_by, modified_time)",
-                                "VALUES ('${permission}', 1, now(), 1, now());"
+                                "INSERT INTO sys_permission (name, created_time, modified_time)",
+                                "VALUES ('${permission}', now(), now());"
                             )
                         }
 
                         appendBlock(
                             """
-INSERT INTO sys_role_sys_permission_mapping (sys_role_id, sys_permission_id)
+INSERT INTO sys_role_sys_permission_mapping (role_id, permission_id)
 SELECT (SELECT id FROM sys_role WHERE name = '${role}' LIMIT 1), id FROM sys_permission 
 WHERE sys_permission.name IN (${permissions.joinToString(", ") { permission -> "'$permission'" }});
 """
@@ -149,7 +149,7 @@ WHERE sys_permission.name IN (${permissions.joinToString(", ") { permission -> "
                         if (it.subGroup != null) {
                             appendBlock(
                                 """
-INSERT INTO sys_role_sys_permission_mapping (sys_role_id, sys_permission_id)
+INSERT INTO sys_role_sys_permission_mapping (role_id, permission_id)
 SELECT (SELECT id FROM sys_role WHERE name = '${subGroupRole}' LIMIT 1), id FROM sys_permission 
 WHERE sys_permission.name IN (${permissions.joinToString(", ") { permission -> "'$permission'" }});
 """
