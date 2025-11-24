e2c59a89049112757ad5d1f253703d86b89115a2
Potmot <377029227@qq.com>
2025-11-24 16:00:06 +0800

    UPDATE: PostgreSQLMetadataFetcher test more type

--- src/test/kotlin/top/potmot/utils/database/metadata/PostgreSQLMetadataFetchTest.kt
+++ src/test/kotlin/top/potmot/utils/database/metadata/PostgreSQLMetadataFetchTest.kt
@@ -80,7 +80,6 @@ class PostgreSQLMetadataFetchTest {
         assert(testTable != null)
         testTable?.apply {
             Assertions.assertEquals("测试表", testTable.comment)
-            Assertions.assertEquals(29, testTable.columns.size)
             Assertions.assertLinesMatch(
                 """
 id 自增主键 integer 10,null PRIMARY AUTO_INCREMENT DEFAULT nextval('test_table_id_seq'::regclass) 
@@ -92,26 +91,50 @@ name 名称 character varying(50) 50,null NULL
 email 邮箱 character varying(100) 100,null NULL
 status 状态 smallint 5,null NULL DEFAULT 1 
 type_int 整数类型 integer 10,null NULL
+type_int2  smallint 5,null NULL
+type_int4  integer 10,null NULL
+type_int8  bigint 19,null NULL
+type_integer  integer 10,null NULL
 type_int_array 整数数组类型 integer[] 10,null NULL
 type_int_array_fixed_size 整数定长数组类型 integer[] 10,null NULL
 type_int_matrix22 二维定长整数数组类型 integer[] 10,null NULL
-type_bigint 大整数类型 bigint 19,null NULL
 type_smallint 小整数类型 smallint 5,null NULL
-type_decimal 精确小数类型 numeric(10,2) 10,2 NULL
-type_decimal_array 精确小数数组类型 numeric(10,2)[] 10,2 NULL
-type_float 单精度浮点数 real 8,8 NULL
+type_bigint 大整数类型 bigint 19,null NULL
+type_numeric 精确小数类型 numeric null,null NULL
+type_numeric_10  numeric(10,0) 10,null NULL
+type_numeric_10_2  numeric(10,2) 10,2 NULL
+type_numeric_10_array 精确小数数组类型 numeric(10,0)[] 10,null NULL
+type_decimal  numeric null,null NULL
+type_decimal_10  numeric(10,0) 10,null NULL
+type_decimal_10_2  numeric(10,2) 10,2 NULL
+type_dec  numeric null,null NULL
+type_dec_10  numeric(10,0) 10,null NULL
+type_dec_10_2  numeric(10,2) 10,2 NULL
+type_real  real 8,8 NULL
+type_float 单精度浮点数 double precision 17,17 NULL
+type_float4  real 8,8 NULL
+type_float8  double precision 17,17 NULL
 type_double 双精度浮点数 double precision 17,17 NULL
 type_boolean 布尔类型 boolean 1,null NULL
 type_date 日期类型 date 13,null NULL
-type_datetime 日期时间类型 timestamp without time zone 29,6 NULL
-type_timestamp 时间戳类型 timestamp without time zone 29,6 NULL DEFAULT CURRENT_TIMESTAMP 
+type_time 时间类型 time without time zone 15,6 NULL
+type_time_tz  time with time zone 21,6 NULL
+type_time_3  time(3) without time zone 12,3 NULL
+type_time_3_tz  time(3) with time zone 18,3 NULL
+type_timestamp 时间戳类型 timestamp without time zone 29,6 NULL
+type_timestamp_3  timestamp(3) without time zone 26,3 NULL
+type_timestamp_default  timestamp without time zone 29,6 NULL DEFAULT CURRENT_TIMESTAMP 
 type_timestamp_tz 时区时间戳类型 timestamp with time zone 35,6 NULL
+type_timestamp_3_tz  timestamp(3) with time zone 32,3 NULL
 type_text 文本类型 text 2147483647,null NULL
+type_uuid  uuid 2147483647,null NULL
 type_check_enum 枚举类型检查 character varying(20) 20,null NULL
+type_char  character(20) 20,null NULL
+type_nchar  character(20) 20,null NULL
 type_json JSON数据类型 json 2147483647,null NULL
 type_jsonb  jsonb 2147483647,null NULL
-type_blob 二进制大对象类型 bytea 2147483647,null NULL
 type_bit 位类型 bit(8) 8,null NULL
+type_bytea 二进制大对象类型 bytea 2147483647,null NULL
                 """.trim().split("\n"),
                 testTable.columns.map { it.stringify() }
             )
--- src/test/resources/database/postgresql.sql
+++ src/test/resources/database/postgresql.sql
@@ -29,26 +29,50 @@ CREATE TABLE test_table
     email                     VARCHAR(100),
     status                    SMALLINT  DEFAULT 1,
     type_int                  INT,
+    type_int2                 INT2,
+    type_int4                 INT4,
+    type_int8                 INT8,
+    type_integer              INTEGER,
     type_int_array            INT[],
     type_int_array_fixed_size INT[10],
     type_int_matrix22         INT[2][2],
-    type_bigint               BIGINT,
     type_smallint             SMALLINT,
-    type_decimal              NUMERIC(10, 2),
-    type_decimal_array        NUMERIC(10, 2)[],
-    type_float                REAL,
+    type_bigint               BIGINT,
+    type_numeric              NUMERIC,
+    type_numeric_10           NUMERIC(10),
+    type_numeric_10_2         NUMERIC(10, 2),
+    type_numeric_10_array     NUMERIC(10) ARRAY[10],
+    type_decimal              DECIMAL,
+    type_decimal_10           DECIMAL(10),
+    type_decimal_10_2         DECIMAL(10, 2),
+    type_dec                  DEC,
+    type_dec_10               DEC(10),
+    type_dec_10_2             DEC(10, 2),
+    type_real                 REAL,
+    type_float                FLOAT,
+    type_float4               FLOAT4,
+    type_float8               FLOAT8,
     type_double               DOUBLE PRECISION,
     type_boolean              BOOLEAN,
     type_date                 DATE,
-    type_datetime             TIMESTAMP,
-    type_timestamp            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    type_timestamp_tz         TIMESTAMP WITH TIME ZONE,
+    type_time                 TIME,
+    type_time_tz              TIME WITH TIME ZONE,
+    type_time_3               TIME(3),
+    type_time_3_tz            TIME(3) WITH TIME ZONE,
+    type_timestamp            TIMESTAMP,
+    type_timestamp_3          TIMESTAMP(3),
+    type_timestamp_default    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type_timestamp_tz         TIMESTAMPTZ,
+    type_timestamp_3_tz       TIMESTAMP(3) WITH TIME ZONE,
     type_text                 TEXT,
+    type_uuid                 UUID,
     type_check_enum           VARCHAR(20),
+    type_char                 CHAR(20),
+    type_nchar                NCHAR(20),
     type_json                 JSON,
     type_jsonb                JSONB,
-    type_blob                 BYTEA,
     type_bit                  BIT(8),
+    type_bytea                BYTEA,
 
     -- 外键约束
     CONSTRAINT fk_user
@@ -94,17 +118,17 @@ COMMENT ON COLUMN test_table.type_int_array_fixed_size IS '整数定长数组类
 COMMENT ON COLUMN test_table.type_int_matrix22 IS '二维定长整数数组类型';
 COMMENT ON COLUMN test_table.type_bigint IS '大整数类型';
 COMMENT ON COLUMN test_table.type_smallint IS '小整数类型';
-COMMENT ON COLUMN test_table.type_decimal IS '精确小数类型';
-COMMENT ON COLUMN test_table.type_decimal_array IS '精确小数数组类型';
+COMMENT ON COLUMN test_table.type_numeric IS '精确小数类型';
+COMMENT ON COLUMN test_table.type_numeric_10_array IS '精确小数数组类型';
 COMMENT ON COLUMN test_table.type_float IS '单精度浮点数';
 COMMENT ON COLUMN test_table.type_double IS '双精度浮点数';
 COMMENT ON COLUMN test_table.type_boolean IS '布尔类型';
 COMMENT ON COLUMN test_table.type_date IS '日期类型';
-COMMENT ON COLUMN test_table.type_datetime IS '日期时间类型';
+COMMENT ON COLUMN test_table.type_time IS '时间类型';
 COMMENT ON COLUMN test_table.type_timestamp IS '时间戳类型';
 COMMENT ON COLUMN test_table.type_timestamp_tz IS '时区时间戳类型';
 COMMENT ON COLUMN test_table.type_text IS '文本类型';
 COMMENT ON COLUMN test_table.type_check_enum IS '枚举类型检查';
 COMMENT ON COLUMN test_table.type_json IS 'JSON数据类型';
-COMMENT ON COLUMN test_table.type_blob IS '二进制大对象类型';
+COMMENT ON COLUMN test_table.type_bytea IS '二进制大对象类型';
 COMMENT ON COLUMN test_table.type_bit IS '位类型';
